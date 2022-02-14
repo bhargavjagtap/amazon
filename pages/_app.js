304a266c89673e4d9ed7f0a6775586a46440bc7d
bhargavjagtap <58814000+bhargavjagtap@users.noreply.github.com>
2022-02-14 15:12:35 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
+import { StoreProvider } from '../utils/Store';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  //Step 1 to get rid of the issue that is css not applied to the page
+  //Step 2 create _document.js 
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {  
+      jssStyles.parentElement.removeChild(jssStyles);
+  }
+},[]);
+
+return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
 export default MyApp
